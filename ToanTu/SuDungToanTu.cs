537282a591ebf7f741078f1d0e7ba80870383687
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-16 13:42:21 +0700

    Add files via upload

---
+++ ToanTu/SuDungToanTu.cs
@@ -0,0 +1,10 @@
+﻿float width;
+float height; 
+
+Console.Write("Nhập vào chiều dài: ");
+width = float.Parse(Console.ReadLine());
+Console.Write("Nhập chiều cao: ");
+height = float.Parse(Console.ReadLine());
+
+float area = width * height;
+Console.WriteLine("Diện tích là: " + area);
