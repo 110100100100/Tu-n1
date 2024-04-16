d2dedc90ab9e63bdbbddbc9d840ae52639721d44
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-16 13:25:24 +0700

    Add files via upload

---
+++ ChuyenDoiTienTe/ChuyenDoiTienTe.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Nhập số tiên bạn muốn chuyển đổi: ");
+string money = Console.ReadLine();
+int USD = Convert.ToInt32(money);
+int VND = USD * 24000;
+
+System.Console.WriteLine(VND);
